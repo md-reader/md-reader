202459d0e96d0b5fe0a30bd8ae8c212066de6d4a
fanbj <fanbj@getui.com>
2019-04-12 11:08:06 +0800

    feat toggle code&markdown

--- inject.js
+++ inject.js
@@ -6,9 +6,11 @@ const {
   anchorTo
 } = utils
 const DEFAULT_MD_CSS_URL = 'css/md-css-github.css'
+let SHOW_CODE = false
 
 const mdCodeEle = BODY.firstElementChild
 const mdCode = mdCodeEle.textContent
+mdCodeEle.classList.add('md-code-wrap')
 mdCodeEle.style.display = 'none'
 
 // inject styles
@@ -16,9 +18,12 @@ void[
   DEFAULT_MD_CSS_URL,
   'css/hljs-style-atom-one-dark.css',
   'css/index.css',
+  'css/btn.css',
 ].forEach(injectStyle)
 
-HEAD.appendChild(createEle('meta', {charset: "UTF-8"}))
+HEAD.appendChild(createEle('meta', {
+  charset: "UTF-8"
+}))
 HEAD.appendChild(createEle('link', {
   rel: "shortcut icon",
   href: getAssetsURL("images/icon128.png")
@@ -70,3 +75,25 @@ const handleNavItem = (ele, i) => {
 headList.forEach(handleNavItem)
 
 BODY.insertBefore(sidebar, BODY.firstElementChild)
+
+
+// toggle code
+const topBarEle = createEle('div', {
+  className: 'top-bar'
+})
+const btn = createEle('button', {
+  className: 'btn edit-btn',
+  title: '编辑'
+})
+
+const editBtn = btn.cloneNode()
+editBtn.onclick = toggleMode
+topBarEle.appendChild(editBtn)
+BODY.insertBefore(topBarEle, BODY.firstElementChild)
+
+function toggleMode() {
+  SHOW_CODE = !SHOW_CODE
+  articleEle.style.display = sidebar.style.display =
+    SHOW_CODE ? 'none' : ''
+  mdCodeEle.style.display = SHOW_CODE ? '' : 'none'
+}
