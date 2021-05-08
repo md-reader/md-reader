80fbb8cd458942744d58b0b2d267d344d7d14ce4
Bener <2388697970@qq.com>
2021-05-08 10:42:28 +0800

    feat: zip build

---
+++ build/zip.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const { resolve } = require('path')
+
+const entryDir = resolve(__dirname, '../extension')
+const outputDir = resolve(__dirname, '../extension.zip')
+const hasDir = fs.existsSync(entryDir)
+
+if (hasDir) {
+  console.log(`zip: "${entryDir}"`)
+  try {
+    const output = fs.createWriteStream(outputDir)
+    const archive = require('archiver')('zip', {
+      zlib: { level: 9 },
+    })
+
+    archive.on('error', console.error)
+
+    output.on('close', () => {
+      console.log(`output: "${outputDir}"`)
+      console.log(archive.pointer() + ' total bytes')
+    })
+
+    archive.pipe(output)
+    archive.directory(entryDir, false)
+    archive.finalize()
+  } catch (err) {
+    console.error(err)
+  }
+}
