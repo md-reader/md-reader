cff4c90d746157fa4edc4c8285b3e15d900cbb8e
2jun0 <soo28819@naver.com>
2021-11-04 02:44:03 +0900

    refactoring: rename the confusing name.
     - Since the locale is a language code, it is necessary to rename the function `locale` in `app.svelte`. (I don't know what `locale` do.)
    
     - For the same reason, a static type was specified in the `i18n` function.

--- src/popup/i18n/index.ts
+++ src/popup/i18n/index.ts
@@ -1,17 +1,16 @@
-import localeMap from './locale.json'
+import localeJson from './locale.json'
 
-/**
- * Unmatched languages (in locale.json file) will default to the 'en'.
- */
-export const DEFAULT_LANG = 'en'
+/** Unmatched languages (in locale.json file) will default to the 'en'.*/
+const DEFAULT_LOCALE: string = 'en';
 
-export default function i18n(lang = DEFAULT_LANG) {
-  const defaultLocaleMap = localeMap[DEFAULT_LANG]
-  const locale = localeMap[lang] || defaultLocaleMap
-  return function getLocale(field) {
+export default function i18n(locale: string = DEFAULT_LOCALE): (field: string)=>string {
+  const defaultLocalizeMap :Object = localeJson[DEFAULT_LOCALE]
+  const localizeMap        :Object = localeJson[locale] || defaultLocalizeMap
+  return function localize(field: string): string {
     // Also include fields
-    return locale[field] || defaultLocaleMap[field] || field
+    return localizeMap[field] || defaultLocalizeMap[field] || field
   }
 }
 
-i18n.localeMap = localeMap
+i18n.localeJson = localeJson
+i18n.DEFAULT_LOCALE = DEFAULT_LOCALE
