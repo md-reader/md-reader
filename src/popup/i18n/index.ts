fadf51fdcbebc33226ffca72c8ed9aba76463251
2jun0 <soo28819@naver.com>
2021-11-05 12:14:18 +0900

    refact: i18n
    - The browser locale value was handled at 'i18n'
    - the locale value could be obtained from `localize` function (due to the future update).
    - The localejson keys value(locales) was handled at 'i18n'

--- src/popup/i18n/index.ts
+++ src/popup/i18n/index.ts
@@ -2,17 +2,36 @@ import localeJson from './locale.json'
 
 /* Unmatched languages (in locale.json file) default use the 'en'. */
 const DEFAULT_LOCALE: string = 'en'
+/* The default locale set in your browser. */
+const BROWSER_LOCALE: string = chrome.i18n.getUILanguage()
 
-export default function i18n(
-  locale: string = DEFAULT_LOCALE,
-): (field: string) => string {
-  const defaultLocalizeMap: Object = localeJson[DEFAULT_LOCALE]
-  const localizeMap: Object = localeJson[locale] || defaultLocalizeMap
-  return function localize(field: string): string {
-    // Also includes fields
+interface Localize {
+  (field: string): string
+  locale: string
+}
+
+interface I18n {
+  (locale?: string): Localize
+  localeJson: Object
+  locales: string[]
+  DEFAULT_LOCALE: string
+}
+
+const i18n: I18n = (
+  locale: string = localeJson[BROWSER_LOCALE] ? BROWSER_LOCALE : DEFAULT_LOCALE,
+): Localize => {
+  const defaultLocalizeMap: Localize = localeJson[DEFAULT_LOCALE]
+  const localizeMap: Localize = localeJson[locale]
+
+  const localize: Localize = (field: string): string => {
+    /* Also includes fields (default 'en') */
     return localizeMap[field] || defaultLocalizeMap[field] || field
   }
+  localize.locale = locale
+
+  return localize
 }
 
 i18n.localeJson = localeJson
+i18n.locales = Object.keys(i18n.localeJson)
 i18n.DEFAULT_LOCALE = DEFAULT_LOCALE
