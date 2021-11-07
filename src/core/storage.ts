dae25ac6bacc6bd09f418a5cd26b4268379bce58
Bener <fanbj@cloudglab.com>
2021-11-08 00:27:32 +0800

    fix: update type

--- src/core/storage.ts
+++ src/core/storage.ts
@@ -1,23 +1,18 @@
-import Data from './data'
+import type Data from './data'
 
-class Storage {
+class Storage<T> {
   local: chrome.storage.LocalStorageArea
   constructor() {
     this.local = chrome.storage.local
   }
-  set(data: Data): Promise<any> {
-    console.log('save', data)
+  set(data: T): Promise<T> {
     return new Promise((resolve) => {
       this.local.set(data, resolve.bind(null, data))
     })
   }
-  get(keys: keyof Data[] | keyof Data = null): Promise<Data> {
-    return new Promise((resolve) =>
-      this.local.get(keys, (data: Data) => {
-        resolve(data)
-      }),
-    )
+  get(keys: keyof T[] | keyof T = null): Promise<T> {
+    return new Promise((resolve) => this.local.get(keys, resolve))
   }
 }
 
-export default new Storage()
+export default new Storage<Data>()
