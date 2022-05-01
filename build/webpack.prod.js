0bc58a75f0456fe725b8a094539cb742483e7075
Bener <benjavan@163.com>
2022-05-01 13:17:30 +0800

    fix: output ASCII

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,21 @@
 const merge = require('webpack-merge')
 const commentConfig = require('./webpack.common.js')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(commentConfig, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+          output: {
+            ascii_only: true,
+          },
+        },
+      }),
+    ],
+  },
 })
