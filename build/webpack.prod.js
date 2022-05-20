67ba02abb3d3d4e8f8b0675c9e03cad43d341fa8
Bener <benjavan@163.com>
2022-05-20 16:47:41 +0800

    fix: minify content css

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,12 +1,14 @@
 const merge = require('webpack-merge')
 const commentConfig = require('./webpack.common.js')
 const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = merge(commentConfig, {
   mode: 'production',
   optimization: {
     minimize: true,
     minimizer: [
+      new CssMinimizerPlugin(),
       new TerserPlugin({
         parallel: true,
         terserOptions: {
