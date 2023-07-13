2fe89d11f7e3ff8309bb4709f852b858a14cfa52
Bener <benjavan@163.com>
2023-07-13 11:01:29 +0800

    feat: popup add about page

---
+++ src/popup/components/about.svelte
@@ -0,0 +1,88 @@
+<script>
+  import Tooltip, { Wrapper } from '@smui/tooltip'
+  import Icon from './icon.svelte'
+  import logoIcon from '@/images/logo.svg'
+  import chromeIcon from '@/images/chrome-web-store.svg'
+  import githubIcon from '@/images/github.svg'
+  import emailIcon from '@/images/email.svg'
+  export let homepage
+  export let version
+</script>
+
+<Icon class="logo-icon" svg={logoIcon} />
+<h1>md-reader</h1>
+<div class="links">
+  <Wrapper>
+    <a target="_blank" href={homepage}>
+      <Icon class="icon-link" svg={chromeIcon} />
+    </a>
+    <Tooltip xPos="start">Chrome Web Store</Tooltip>
+  </Wrapper>
+  <Wrapper>
+    <a target="_blank" href="https://github.com/md-reader/md-reader">
+      <Icon class="icon-link" svg={githubIcon} />
+    </a>
+    <Tooltip xPos="start">GitHub</Tooltip>
+  </Wrapper>
+  <Wrapper>
+    <a href="mailto:mkdreader@gmail.com">
+      <Icon class="icon-link" svg={emailIcon} />
+    </a>
+    <Tooltip xPos="start">Email</Tooltip>
+  </Wrapper>
+</div>
+<div class="footer">
+  <div class="version">v{version}</div>
+  <div class="copyright">
+    Copyright &copy;2018-{new Date().getFullYear()} Bener.
+  </div>
+</div>
+
+<style>
+  :global(.logo-icon) {
+    display: block !important;
+    width: 20%;
+    margin: 60px auto 10px;
+  }
+  h1 {
+    text-align: center;
+    line-height: 1;
+    font-size: 24px;
+    margin: 0;
+    color: #35363a;
+  }
+  .links {
+    text-align: center;
+    font-size: 0;
+    margin-top: 40px;
+  }
+  .links a {
+    display: inline-block;
+    transition: 0.2s;
+    color: #35363a;
+  }
+  .links a:not(:last-child) {
+    margin-right: 12px;
+  }
+  .links a:hover {
+    opacity: 0.6;
+  }
+  .links :global(.icon-link) {
+    display: inline-block;
+    height: 22px;
+  }
+  .footer {
+    font-size: 12px;
+    opacity: 0.3;
+  }
+  .version {
+    position: absolute;
+    right: 10px;
+    bottom: 8px;
+  }
+  .copyright {
+    position: absolute;
+    bottom: 8px;
+    left: 10px;
+  }
+</style>
--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,11 +1,11 @@
 import App from './components/app.svelte'
 import './index.css'
-import '@smui/radio/bare.css'
 import '@smui/switch/bare.css'
 import '@smui/button/bare.css'
-import '@smui/chips/bare.css'
+import '@smui/tooltip/bare.css'
 import '@smui/select/bare.css'
 import '@smui/form-field/bare.css'
+import '@smui/segmented-button/bare.css'
 
 const app = new App({
   target: document.body,
