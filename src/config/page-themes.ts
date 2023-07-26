ebc63ff05625245ca8b9c2cb919fe761feedcd14
Bener <benjavan@163.com>
2023-07-26 21:49:52 +0800

    fix: fixed inconsistencies in scrollbar themes #42

--- src/config/page-themes.ts
+++ src/config/page-themes.ts
@@ -1,4 +1,5 @@
 export type Theme = 'light' | 'dark' | 'auto'
 
-export const themePrefix = 'page-theme--'
+export const rootThemePrefix = 'mdReaderTheme'
+export const rootThemeDisabledPrefix = 'mdReaderThemeSchemeDisabled'
 export default <Theme[]>['light', 'dark', 'auto']
